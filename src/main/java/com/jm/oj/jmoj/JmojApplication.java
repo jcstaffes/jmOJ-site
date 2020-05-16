f8e765bc7f64982c27c7047ae7a8c41266bef8b8
u6801714 <u6801714@anu.edu.au>
2020-05-16 20:58:15 +1000

    Initial commit

---
+++ src/main/java/com/jm/oj/jmoj/JmojApplication.java
@@ -0,0 +1,13 @@
+package com.jm.oj.jmoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jm/oj/jmoj/JmojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.oj.jmoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
